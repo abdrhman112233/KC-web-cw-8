5db46869f8340981a678e9ef3b7eb78f3244a03b
abdrhman112233 <109982638+abdrhman112233@users.noreply.github.com>
2022-08-20 14:07:10 +0300

    Done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let grades = [90, 94, 65, 80, 39, 55, 83, 92, 39, 93, 43, 70, 57, 0];
+let grades = [90, 94, 65, 80, 39, 55, 83, 92, 39, 93, 43, 70, 57,0];
 
 let all_grades_div = document.getElementById("all-grades");
 let search_grades_div = document.getElementById("search-grades");
@@ -8,22 +8,34 @@ let random_grade_div = document.getElementById("random-grade");
 // لا تقم بتغيير او تخريب الكود الموجود فوق هذا الكومنت
 
 // طبّق الخطوة 4 في أسفل هذا الكومنت
-
+grades.pop()
 // طبّق الخطوة 5 في أسفل هذا الكومنت
-
+grades.push(99)
 // طبّق الخطوة 6 أسفل هذا الكومنت
+for (let x = 0; x < grades.length; x++) {
+  const element = grades[x];
+  all_grades_div.innerHTML += `<div>${element}</div>`;
 
-
+}
 
 function search() {
   let inputValue = document.getElementById("input").value;
   
   // طبّق الخطوة 7 أسفل هذا الكومنت
-  
+let filteredGrades=  grades.filter((x) => x == inputValue);
+console.log(filteredGrades)
+if (filteredGrades ==0) {
+
+console.log (`الدرجة غير موجوده`)
+
 }
 
-// طبّق الخطوة 8 و 9 أسفل هذا الكومنت
 
+
+}
+// طبّق الخطوة 8 و 9 أسفل هذا الكومنت
+let randomIndex= Math.floor(Math.random() * grades.length)
 // طبّق الخطوة 10 أسفل هذا الكومنت
 
+random_grade_div.innerHTML = `<div>${grades[randomIndex]}</div>`;
 
